07c3cf00363a731308d7c3a7dd07f608fd0b94b0
Alex D <alex.dyous@sdgku.edu>
2026-03-10 17:36:41 -0500

    Initial Commit

---
+++ ToDoTracker/ToDoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTrackerApp.swift
+//  ToDoTracker
+//
+//  Created by Alex Arthur on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoTrackerTests/ToDoTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTrackerTests.swift
+//  ToDoTrackerTests
+//
+//  Created by Alex Arthur on 3/10/26.
+//
+
+import Testing
+@testable import ToDoTracker
+
+struct ToDoTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
