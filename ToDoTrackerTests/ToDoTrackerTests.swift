e917fd8d6854958d6939f4c40f6311c2de6b70c1
Alex Dyous <238120430+alexd0999@users.noreply.github.com>
2026-04-17 14:57:31 -0500

    Implement unit tests for task priority and addition
    Added unit tests for task priority and adding tasks to a group.

--- ToDoTrackerTests/ToDoTrackerTests.swift
+++ ToDoTrackerTests/ToDoTrackerTests.swift
@@ -5,13 +5,36 @@
 //  Created by Alex Arthur on 3/5/26.
 //
 
-import Testing
+import XCTest
+//imports main app code so the test file can read
 @testable import ToDoTracker
 
-struct ToDoTrackerTests {
+final class ToDoTrackerTests: XCTestCase {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    // A: TDD Requirement - test if a task has a priority
+    func testTaskHasPriority() {
+        //create task
+        var task = TaskItem(title: "Complete Homework")
+        
+        //assign it a priority (inevitable error)
+        task.priority = .high
+        
+        //checks that priority successfully saved as .high
+        XCTAssertEqual(task.priority, .high)
     }
-
+    
+    // B: Unit Test Requirement -  test adding a task
+    func testAddingTask() {
+        //create blank group
+        var workGroup = TaskGroup(title: "Work", symbolName: "briefcase", tasks: [])
+        
+        //add task to group
+        let newTask = TaskItem(title: "Buy Groceries")
+        workGroup.tasks.append(newTask)
+        
+        //checks group item count
+        XCTAssertEqual(workGroup.tasks.count, 1)
+        //checks item name matches input
+        XCTAssertEqual(workGroup.tasks[0].title, "Buy Groceries")
+    }
 }
